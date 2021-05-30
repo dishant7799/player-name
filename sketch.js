7bb7b82c5c8fa33c1a0fcafe94565bcaec5f7ce5
Dishant <78219380+dishant7799@users.noreply.github.com>
2021-05-30 21:11:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gameState=0;
+var database;
+var playerCount;
+var form,player,game;
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+game = new Game();
+game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  
+}
